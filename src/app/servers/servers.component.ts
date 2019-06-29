543c787bd9c1f162ebd749f20b81c9985d0ddb38
abondr <abondr@gmail.com>
2019-06-29 16:41:43 +0530

    oneWayBinding class to html

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Server } from 'selenium-webdriver/safari';
 
 @Component({
   selector: 'app-servers',
@@ -6,8 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
+  serverObj: Server[] = [];
+  index:number =0;
+  stat:boolean = true;
+  constructor() {
+    setInterval(
+      () => {
+        this.index += 1;
+        this.stat = this.stat === true? false : true;
+        this.serverObj.push({'number':this.index,'status':this.stat});
+        console.log({'number':this.index,'status':this.stat});
+     }, 2000);
+  }
 
   ngOnInit() {
   }
